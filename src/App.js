199884e2d0476276506241557262b3e63d415839
Ivana Livaja <ivanalivaja@maia.global>
2024-08-06 15:15:56 +0200

    update matching game

--- src/App.js
+++ src/App.js
@@ -1,137 +1,168 @@
-import React, { Component } from 'react';
-import Card from './Card';
-import Deck from './Deck';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Deck from "./Deck";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faRedo,
-	faStar,
-	faLeaf,
-	faPaperPlane,
-	faAnchor,
-	faPaw,
-	faBolt,
-	faRocket,
-	faBicycle,
-	faBomb
-} from '@fortawesome/free-solid-svg-icons';
+  faRedo,
+  faStar,
+  faLeaf,
+  faPaperPlane,
+  faAnchor,
+  faPaw,
+  faBolt,
+  faRocket,
+  faBicycle,
+  faBomb,
+} from "@fortawesome/free-solid-svg-icons";
+import swal from "sweetalert";
 
-import './App.css';
+import "./App.css";
 
-library.add(faStar, faRedo, faLeaf, faPaperPlane, faAnchor, faPaw, faBolt, faRocket, faBicycle, faBomb);
+library.add(
+  faStar,
+  faRedo,
+  faLeaf,
+  faPaperPlane,
+  faAnchor,
+  faPaw,
+  faBolt,
+  faRocket,
+  faBicycle,
+  faBomb
+);
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			cards: [],
-			matched: [],
-			flipped: [],
-			counter: 0,
-			disabled: false
-		};
-	}
-	UNSAFE_componentWillMount() {
-		this.newGame();
-	}
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [matched, setMatched] = useState([]);
+  const [firstCard, setFirstCard] = useState(null);
+  const [secondCard, setSecondCard] = useState(null);
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const [isCalled, setIsCalled] = useState(false);
 
-	//Durstenfeld shuffle
-	shuffle = (array) => {
-		let newArr = array.slice();
-		for (let i = array.length - 1; i > 0; i--) {
-			let j = Math.floor(Math.random() * (i + 1));
-			[ newArr[i], newArr[j] ] = [ newArr[j], newArr[i] ];
-		}
-		return newArr;
-	};
+  //Durstenfeld shuffle
+  const shuffle = (array) => {
+    let newArr = array.slice();
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+    return newArr;
+  };
 
-	newGame = () => {
-		this.setState({
-			cards: this.shuffle(Deck()),
-			matched: [],
-			flipped: [],
-			counter: 0,
-			disabled: false
-		});
-	};
+  const newGame = () => {
+    setCards(shuffle(Deck()));
+    setMatched([]);
+    setFirstCard(null);
+    setSecondCard(null);
+    setCounter(0);
+    setDisabled(false);
+    setIsCalled(true);
+  };
 
-	match = (id) => {
-		const { cards, flipped } = this.state;
-		const clickedCard = cards.find((card) => card.id === id);
-		const flippedCard = cards.find((card) => flipped[0] === card.id);
-		return flippedCard.icon === clickedCard.icon;
-	};
+  //checking if id matches with flipped card
+  const match = (id) => {
+    const clickedCard = cards.find((card) => card.id === id);
+    const flippedCard = cards.find((card) => firstCard === card.id);
+    return flippedCard?.icon === clickedCard?.icon;
+  };
 
-	handleClick = (id) => {
-		const { matched, flipped, counter } = this.state;
+  useEffect(() => {
+    if (!isCalled) {
+      newGame();
+    }
+  }, []);
 
-		this.setState({
-			disabled: true
-		});
-		const sameCardClicked = (id) => {
-			this.state.flipped.includes(id);
-		};
+  useEffect(() => {
+    if (matched.length === 16) {
+      swal(
+        "Good job!",
+        `You won the game with ${counter} moves! Can you do it with less moves?`,
+        "success",
+        {
+          button: "Play again!",
+        }
+      ).then((result) => {
+        if (result) {
+          newGame();
+        }
+      });
+    }
+  }, [matched]);
 
-		if (flipped.length === 0) {
-			this.setState({
-				flipped: [ id ],
-				disabled: false
-			});
-		} else {
-			if (sameCardClicked(id)) return;
+  const handleClick = (id) => {
+    setDisabled(true);
+    if (firstCard === null) {
+      setFirstCard(id);
+      setDisabled(false);
+    } else {
+      if (id === firstCard) {
+        setDisabled(false);
+        return;
+      }
+      setSecondCard(id);
+      setCounter(counter + 1);
+      if (match(id)) {
+        setMatched([...matched, firstCard, id]);
+        resetCards();
+      } else {
+        setTimeout(() => {
+          resetCards();
+        }, 750);
+      }
+    }
+  };
 
-			this.setState({
-				flipped: [ flipped[0], id ],
-				counter: counter + 1
-			});
+  const resetCards = () => {
+    setFirstCard(null);
+    setSecondCard(null);
+    setDisabled(false);
+  };
 
-			if (this.match(id)) {
-				this.setState({
-					matched: [ ...matched, flipped[0], id ]
-				});
-				this.resetCards();
-			} else {
-				setTimeout(this.resetCards, 750);
-			}
-		}
-	};
+  return (
+    <div className="App">
+      <div className="App__container">
+        <h1>Matching Game</h1>
+        <div className="rating">
+          <div className="moves">Moves: {counter}</div>
+          <div
+            className="redo"
+            title="Play Again"
+            onClick={() => {
+              swal({
+                title: "Are you sure you want to reset the game?",
+                text: "If you reset, you will lose your progress!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              }).then((result) => {
+                if (result) {
+                  newGame();
+                }
+              });
+            }}
+          >
+            <FontAwesomeIcon className="redo-icon" icon={faRedo} /> Reset
+          </div>
+        </div>
 
-	resetCards = () => {
-		this.setState({
-			flipped: [],
-			disabled: false
-		});
-	};
-
-	render() {
-		const { cards, matched, disabled, flipped } = this.state;
-
-		return (
-			<div className="App">
-				<h2>Matching Game</h2>
-				<div className="rating">
-					<div className="moves">Moves: {this.state.counter}</div>
-					<div className="redo" title="Play Again" onClick={this.newGame}>
-						<FontAwesomeIcon className="redo-icon" icon={faRedo} />
-					</div>
-				</div>
-
-				<div className="deck" id="card-deck">
-					{cards.map((card) => (
-						<Card
-							key={card.id}
-							handleClick={this.handleClick}
-							id={card.id}
-							icon={card.icon}
-							matched={matched.includes(card.id)}
-							flipped={flipped.includes(card.id)}
-							disabled={disabled || matched.includes(card.id)}
-						/>
-					))}
-				</div>
-			</div>
-		);
-	}
-}
+        <div className="deck" id="card-deck">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              handleClick={handleClick}
+              id={card.id}
+              icon={card.icon}
+              matched={matched.includes(card.id)}
+              flipped={firstCard === card.id || secondCard === card.id}
+              disabled={disabled || matched.includes(card.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "animate.css";
 
-function Card({ id, icon, handleClick, flipped, matched }) {
-	const visible = flipped || matched ? 'front' : 'back';
+function Card({ id, icon, handleClick, flipped, matched, disabled }) {
+  const visible = flipped || matched ? "front" : "back";
 
-	return (
-		<div className={`card ${visible}`} onClick={() => handleClick(id)}>
-			<div className="font-icon">
-				<FontAwesomeIcon icon={icon} className="icon" />
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={`card ${
+        visible === "front"
+          ? `${visible} animate__animated animate__flipInY`
+          : `${visible}`
+      }`}
+      onClick={() => {
+        if (!disabled) handleClick(id);
+      }}
+    >
+      <div className="font-icon">
+        <FontAwesomeIcon icon={icon} className="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Card;
